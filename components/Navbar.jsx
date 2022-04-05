51e75498912cc6265de15e393c5ee2b7c1e1679d
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-04-05 18:22:37 +0530

    feat:added mata mask

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,17 @@
 import Link from "next/link";
+import Web3 from "web3";
 export const Navbar = () => {
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("No wallet installed");
+    }
+  };
+
   return (
     <>
       <div className="flex justify-between px-24 bg-back h-20 items-center">
@@ -24,8 +36,11 @@ export const Navbar = () => {
             <li className="text-grey">
               <Link href="#">Community</Link>
             </li>
-            <li className="bg-blue-main p-3 px-5 rounded-md text-white font-bold">
-              <Link href="#">Connect Wallet</Link>
+            <li
+              onClick={() => loadWeb3()}
+              className="bg-blue-main p-3 px-5 rounded-md text-white font-bold cursor-pointer"
+            >
+              Connect Wallet
             </li>
           </ul>
         </div>
