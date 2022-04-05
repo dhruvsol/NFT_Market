e3b711e8ed48b838b16891d7adeec7139199d86e
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-04-05 19:11:31 +0530

    feat:added wallet address

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
 import Web3 from "web3";
+
 export const Navbar = () => {
+  const [address, setAddress] = useState("Connect Wallet");
+  const [active, setActive] = useState(true);
   const loadWeb3 = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.request({ method: "eth_requestAccounts" });
+      loadData();
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
       window.alert("No wallet installed");
     }
+  };
+
+  const ConnectRef = useRef();
+  const loadData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    setAddress(accounts[0]);
+    console.log(ConnectRef.current.innerHTML);
+    ConnectRef.current.innerHTML = setAddress;
+    setActive(false);
   };
 
   return (
@@ -36,12 +51,24 @@ export const Navbar = () => {
             <li className="text-grey">
               <Link href="#">Community</Link>
             </li>
-            <li
-              onClick={() => loadWeb3()}
-              className="bg-blue-main p-3 px-5 rounded-md text-white font-bold cursor-pointer"
-            >
-              Connect Wallet
-            </li>
+            {active && (
+              <li
+                value={address}
+                ref={ConnectRef}
+                onClick={() => loadWeb3()}
+                className="bg-blue-main p-3 px-5 rounded-md text-white font-bold  cursor-pointer"
+              >
+                {address}
+              </li>
+            )}
+            {active === false && (
+              <li
+                ref={ConnectRef}
+                className="bg-blue-main p-3 px-5 rounded-md text-white font-bold  cursor-pointer"
+              >
+                {address}
+              </li>
+            )}
           </ul>
         </div>
       </div>
